d27e023935df522a1f8fabce0b54a10190d7bb9a
Luiza <mlosantos.cic@uesc.br>
2024-05-15 09:47:41 -0300

    .

--- matriz.c
+++ matriz.c
@@ -5,10 +5,11 @@ int main() {
     int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int i, j;
     int soma = 0;
-
+    int media = 0;
+    
     for(i = 0; i < 3; i++){
         for(j = 0; j < 3; j++){
-            soma += matriz[i][j]);
+            soma += matriz[i][j];
         }
         printf("\n");
     }
@@ -17,6 +18,6 @@ int main() {
 
     printf("soma = %d", soma);
     printf("media = %d", media);
-    
+
     return 0;
 }
