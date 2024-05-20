e28065e074497758a1d2e634e89e3e1c77234546
Luiza <mlosantos.cic@uesc.br>
2024-05-19 22:15:52 -0300

    std

---
+++ python/3.py
@@ -0,0 +1,8 @@
+""" Faça um Programa que peça dois números e imprima a soma. """
+
+numa = int(input("Digite o primeiro numero:"))
+numb = int(input("Digite o segundo numero:"))
+
+soma = numa + numb
+
+print(soma)
---
+++ python/4.py
@@ -0,0 +1,11 @@
+""" Faça um Programa que peça 
+as 4 notas bimestrais e mostre a média """
+
+numa = float(input("Digite o primeiro numero:"))
+numb = float(input("Digite o segundo numero:"))
+numc = float(input("Digite o terceiro numero:"))
+numd = float(input("Digite o quarto numero:"))
+
+media = (numa + numb + numc + numd) / 4
+
+print(media)
